e49fde70f33282d464cefe9cae4bfb692ebc4003
Jefferson Santos <jeffersonscr12@gmail.com>
2022-08-24 21:45:18 -0300

    home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="timerAmount">durante</label>
+          <input id="timerAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,12 +15,14 @@ export const GlobalStyle = createGlobalStyle`
     body{
         background: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-300']};
+        -webkit-font-smoothing: antialiased;
     }
 
     body, input, textarea, button {
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         font-size: 1rem;
+        
     }
 
 `
